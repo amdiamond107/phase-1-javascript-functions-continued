9c56e820914b6eed19cf0d90921a7ca264cd5362
Andrew Diamond <amdiamond107@gmail.com>
2023-04-14 15:26:18 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`
+};
+
+function wrapAdjective (result = "*") {
+    return function(box = "special") {
+        return `You are ${result}${box}${result}!`
+    }
+};
+// wrapAdjective("You are");
+
+// return value = "You are *a hard worker*!"cd
